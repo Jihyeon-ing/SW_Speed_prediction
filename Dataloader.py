f3c8c6b71192d10484f06fcd389ff2fdfcbd9572
Jihyeon Son <snjhyn@khu.ac.kr>
2024-01-17 16:36:58 +0900

    Update Dataloader.py

--- Dataloader.py
+++ Dataloader.py
@@ -27,37 +27,37 @@ class Dataset:
     img_files1 = sorted(glob.glob(self.img_path1+f'/png{i}/*.png'))
     img_files2 = sorted(glob.glob(self.img_path2+f'/png{i}/*.png'))
 		
-	# sw input
-	sw_x = []          # a list for the input sw speeds
-	input_dates = []   # a list for input time
-	
-	for j in ragne(len(sw_speed)-(self.input_seq_sw+self.output_seq)+1):
-		sw_x.append(sw_speed[j:j+self.input_seq_sw])
-		input_dates.append(date[j:j+self.input_seq_sw])
-	
-	# img input
-	img_x = []         
-	# a list for input solar images (save only file name
-	# Images are loaded in "DataGenerator" during training (or test))
-	
-	for j in range(len(img_files1)-(self.input_seq_sw+self.output_seq)+1):
-		temp1 = []
-		temp2 = []
-		for k in range(0, self.input_seq_sw, self.input_seq_sw/self.input_seq_img):
-			temp1.append(img_files1[j+k])
-			temp2.append(img_files2[j+k])
-		img_x.append([temp1, temp2])
-		# shape of img_x --> (N, input_seq_img, 2)
-	
-	# target
-	y = []             # a list for target sw speeds
-	target_dates = []  # a list for target time
-	
-	for j in ragne(self.input_seq_sw, len(sw_speed)-self.output_seq+1):
-				y.append(sw_speed[j:j+self.output_seq])
-				target_dates.append(date[j:j+self.output_seq])
-	
-	return sw_x, y, img_x, input_dates, target_dates
+  	# sw input
+  	sw_x = []          # a list for the input sw speeds
+  	input_dates = []   # a list for input time
+  	
+  	for j in ragne(len(sw_speed)-(self.input_seq_sw+self.output_seq)+1):
+  		sw_x.append(sw_speed[j:j+self.input_seq_sw])
+  		input_dates.append(date[j:j+self.input_seq_sw])
+  	
+    # img input
+    img_x = []         
+    # a list for input solar images (save only file name
+    # Images are loaded in "DataGenerator" during training (or test))
+    
+    for j in range(len(img_files1)-(self.input_seq_sw+self.output_seq)+1):
+      temp1 = []
+      temp2 = []
+      for k in range(0, self.input_seq_sw, self.input_seq_sw/self.input_seq_img):
+        temp1.append(img_files1[j+k])
+        temp2.append(img_files2[j+k])
+      img_x.append([temp1, temp2])
+      # shape of img_x --> (N, input_seq_img, 2)
+    
+    # target
+    y = []             # a list for target sw speeds
+    target_dates = []  # a list for target time
+    
+    for j in ragne(self.input_seq_sw, len(sw_speed)-self.output_seq+1):
+          y.append(sw_speed[j:j+self.output_seq])
+          target_dates.append(date[j:j+self.output_seq])
+    
+    return sw_x, y, img_x, input_dates, target_dates
 
 def divide_dataset(self, i):
 	sw_x, y, img_x, input_dates, target_dates = self.get_dataset(i)
